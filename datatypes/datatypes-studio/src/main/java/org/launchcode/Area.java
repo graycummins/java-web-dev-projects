be7872f867e73ec8a5e85c3eae1c73224a3c0bee
graycummins <>
2023-08-21 18:50:35 -0500

    Took out unused variables

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        double pi = 3.14;
         double radius;
         double area;
         Scanner input;
 
         input = new Scanner(System.in);
-        System.out.println("Enter the radius");
+        System.out.println("Enter the circle radius");
         radius = input.nextDouble();
         input.close();
 
