2f9f444e91c9726ab60bf990aa0f710ed168c034
graycummins <>
2023-08-21 16:15:40 -0500

    Completed area and circle class

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,21 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double radius;
+        double area;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter the radius");
+        radius = input.nextDouble();
+        input.close();
+
+        area = Circle.getArea(radius);
+
+        System.out.println("The area is: " +area);
+    }
+}
