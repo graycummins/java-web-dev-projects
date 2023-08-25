494ac01f0ac481f9836ac9ba3a5e3ee3d533857a
graycummins <>
2023-08-24 20:06:14 -0500

    Completed the studio and bonus mission

---
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/counting-characters.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class counting-characters {
+    public static void main(String[] args) {
+        String myString = "some word";
+
+        char[] charactersInString = myString.toCharArray();
+
+        System.out.println(charactersString);
+
+        }
+}
